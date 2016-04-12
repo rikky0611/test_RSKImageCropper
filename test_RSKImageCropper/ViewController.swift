b080cef73b278bbc3b3aa4eecc20d655d9c39d0f
riku arakawa <a_ra_ra_rikky0611@yahoo.co.jp>
2016-04-12 22:45:09 +0900

    Initial Commit

---
+++ test_RSKImageCropper/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  test_RSKImageCropper
+//
+//  Created by 荒川陸 on 2016/04/12.
+//  Copyright © 2016年 Riku Arakawa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
